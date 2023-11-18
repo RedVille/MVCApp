68d543075210da9c49da3ec4720ae481c090060c
yanet <yanet@REDVILLE>
2023-11-18 09:15:20 -0600

    Agregar archivos de proyecto.

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //Get: /HelloWorld/
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        ////Get: /HelloWorld/Welcome/
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    //return "This is the welcome action method...";
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, Numtimes is: {numTimes}");
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
---
+++ MVCApp/Data/MVCAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCApp.Models;
+
+namespace MVCApp.Data
+{
+    public class MVCAppContext : DbContext
+    {
+        public MVCAppContext (DbContextOptions<MVCAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
